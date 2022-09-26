b9111d89be3603b7296c7a305c38276d7d9afdd0
=sofysha91 <sofy.hernandeza@gmail.com>
2022-09-26 18:40:27 -0500

    Header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState} from 'react';
 import styles from "./Header.module.scss";
 import MainNavbar from './MainNavbar/MainNavbar';
@@ -18,27 +17,6 @@ const Header = () => {
         <header className={`${styles.header}`}>
             <MainNavbar handleShow={handleShow} handleShowSearch={handleShowSearch} showSearch={showSearch} showMenu={showMenu} handleShowMenu={handleShowMenu}/>
             <MobileMenu show={show} handleClose={handleClose}/>
-=======
-import React from 'react';
-import LoginBotton from "./LoginBotton/LoginBotton"
-import MenuBotton from "./MenuBotton/MenuBotton"
-import CreateAccountBotton from "./CreateAccountBotton/CreateAccountBotton";
-import styles from "./Header.module.scss"
-import LogoSearchbox from './Logo&Searchbox/Logo&Searchbox';
-
-
-const Header = () => {
-    return (
-        <header className={`dev-${styles.header}`}>
-            <nav className={`${styles.navbar}`}>
-                <div className='container align-items-center'>
-                    <LogoSearchbox/>
-                    <MenuBotton />
-                    <LoginBotton />
-                    <CreateAccountBotton />
-                </div>
-            </nav>
->>>>>>> main
         </header>
     )
 }
